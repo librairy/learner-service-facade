50da41d36823af9c4d270f690dfa6207c4b46abb
Carlos Badenes <cbadenes@gmail.com>
2018-03-06 17:03:53 +0100

    rename classes

--- src/main/java/org/librairy/service/learner/facade/rest/model/ModelParameters.java
+++ src/main/java/org/librairy/service/learner/facade/rest/model/ModelParameters.java
@@ -2,22 +2,23 @@ package org.librairy.service.learner.facade.rest.model;
 
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
  */
 
-public class TrainRequest {
+public class ModelParameters {
 
     @ApiModelProperty(notes="algorithm settings")
-    private Map<String,String> parameters;
+    private Map<String,String> parameters = new HashMap<>();
 
-    public TrainRequest(Map<String,String> parameters) {
+    public ModelParameters(Map<String,String> parameters) {
         this.parameters     = parameters;
     }
 
-    public TrainRequest(){};
+    public ModelParameters(){};
 
     public Map<String, String> getParameters() {
         return parameters;
---
+++ src/main/java/org/librairy/service/learner/facade/rest/model/WordsRequest.java
@@ -0,0 +1,40 @@
+package org.librairy.service.learner.facade.rest.model;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
+ */
+
+public class WordsRequest {
+
+    @ApiModelProperty(notes="max number of words")
+    private Integer maxWords;
+
+    @ApiModelProperty(notes="topic identifier")
+    private Integer topicId;
+
+    public WordsRequest(Integer topicId, Integer maxWords) {
+
+        this.topicId= topicId;
+        this.maxWords = maxWords;
+    }
+
+    public WordsRequest(){};
+
+    public Integer getMaxWords() {
+        return maxWords;
+    }
+
+    public Integer getTopicId() {
+        return topicId;
+    }
+
+    public void setMaxWords(Integer maxWords) {
+        this.maxWords = maxWords;
+    }
+
+    public void setTopicId(Integer topicId) {
+        this.topicId = topicId;
+    }
+}
